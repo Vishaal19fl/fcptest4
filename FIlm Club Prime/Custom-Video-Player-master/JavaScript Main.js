e04071d4b3a0a99bfd6d97ef14ff363535b4421a
Vishaal19fl <79955119+Vishaal19fl@users.noreply.github.com>
2022-01-08 21:51:50 +0530

    Add files via upload

---
+++ FIlm Club Prime/Custom-Video-Player-master/JavaScript Main.js
@@ -0,0 +1,34 @@
+                   </a>
+                              </div>
+                            </div>
+                            <div class="trending-list mt-4">
+                              <div class="text-primary title">
+                                Starring : 
+                                <span class="text-body">
+                                  Pankaj Tripathi, Ali Fazal, Divyendu Sharma 
+                                </span>
+                              </div>
+                              <div class="text-primary title">
+                                Genres :
+                                <span class="text-body">
+                                  Action, Crime, Drama 
+                                </span>
+                              </div>
+                              <div class="text-primary title">
+                                Creators :
+                                <span class="text-body">
+                                  Puneet Krishna, Karan Anshuman
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div id="trending-data22" class="overlay-tab tab-pane fade">
+                          <div class="trending-info align-items-center w-100 animated fadeInUp">
+                            <a href="#" tabindex="0">
+                              <div class="channel-logo">
+                                <img src="images/logo.png" class="c-logo" alt="">
+                              </div>
+                            </a>
+         
